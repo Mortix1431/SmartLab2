895be199d30dc22ad88732967bb34c9e12250aa9
morty <cawa12345333@gmail.com>
2023-06-07 18:45:03 +0600

    first commit

---
+++ src/main/java/me/darkakyloff/smartlab/Splash.java
@@ -0,0 +1,32 @@
+package me.darkakyloff.smartlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity
+{
+    private static final int SPLASH_SCREEN_TIMEOUT = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(Splash.this, Session.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT * 1000);
+
+    }
+}
